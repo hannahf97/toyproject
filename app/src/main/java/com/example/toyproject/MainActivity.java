0a4c9eab8728169087b8653487712a390c3d3ff1
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2021-02-09 17:38:48 +0900

    test2

--- app/src/main/java/com/example/toyproject/MainActivity.java
+++ app/src/main/java/com/example/toyproject/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // test1
+    // test2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
